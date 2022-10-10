e0d083a149704a192ef4c8b5f78afd4f8d52a371
Matthew <112645769+mattygiles@users.noreply.github.com>
2022-10-09 19:44:53 -0600

    HackerRank test – the subject line should read Activision CT-Online / TP Test Invitation

--- sortBinaryNumbers.py
+++ sortBinaryNumbers.py
@@ -6,20 +6,51 @@ import random
 import re
 import sys
 
+
+
+#
 # Complete the 'sortBinaryNumbers' function below.
+#
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts 2D_INTEGER_ARRAY bitArrays as parameter.
+#
 
 
+def compare(index1, index2, bitArrays):
+    for i in range(len(bitArrays[index1])):
+        if (bitArrays[index1][i] > bitArrays[index2][i]):
+            return 1
+        elif (bitArrays[index1][i] < bitArrays[index2][i]):
+            return -1
+    return 0
+            
+
+def compare_to_keys(my_compares, bitArrays):
+    class A:
+        def __init__(self, obj, *args):
+            self.obj = obj
+        def __lt__(self, other):
+            return my_compares(self.obj, other.obj, bitArrays) < 0
+        def __gt__(self, other):
+            return my_compares(self.obj, other.obj, bitArrays) > 0
+        def __eq__(self, other):
+            return my_compares(self.obj, other.obj, bitArrays) == 0
+        def __le__(self, other):
+            return my_compares(self.obj, other.obj, bitArrays) <= 0
+        def __ge__(self, other):
+            return my_compares(self.obj, other.obj, bitArrays) >= 0
+        def __ne__(self, other):
+            return my_compares(self.obj, other.obj, bitArrays) != 0
+    return A
+
+    
 def sortBinaryNumbers(bitArrays):
-    array = [[0, i] for i in range(len(bitArrays))]
-
-    for i in range(len(bitArrays)):
-        for bit in bitArrays[i]:
-            array[i][0] = array[i][0] | (1 << bit)
-
-    array.sort(reverse=True)
-    return [element[1] for element in array]
+    for value in bitArrays:
+        value.sort(reverse = True)
+        
+    array = [i for i in range(len(bitArrays))]
+    array.sort(reverse = True, key=compare_to_keys(compare, bitArrays))
+    return array
 
 
 if __name__ == '__main__':
